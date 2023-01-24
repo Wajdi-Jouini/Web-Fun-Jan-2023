a7d079ab26df9f36819323145c01eb3d4fab4fc1
Wajdi Jouini <jouiniwaj@gmail.com>
2023-01-24 12:24:19 +0100

    Liking Likes

---
+++ Week2/Day 2/Core/Liking Likes Part1/LikingLikes.js
@@ -0,0 +1,6 @@
+var count=3;
+var CountElement= document.querySelector(".nbrLikes");
+function Add1(){
+    count++;
+    CountElement.innerText= count + " Like(s)";
+}
---
+++ Week2/Day 2/Core/Liking Likes Part2/likingLikesPart2.js
@@ -0,0 +1,18 @@
+var count=3;
+var CountElement= document.querySelector(".nbrLikes1");
+function Add1(){
+    count++;
+    CountElement.innerText= count + " Like(s)";
+}
+var count2=12;
+var Count2Element= document.querySelector(".nbrLikes2");
+function Add2(){
+    count2++;
+    Count2Element.innerText= count2 + " Like(s)";
+}
+var count3=9;
+var Count3Element= document.querySelector(".nbrLikes3");
+function Add3(){
+    count3++;
+    Count3Element.innerText= count3 + " Like(s)";
+}
